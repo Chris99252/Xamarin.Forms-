b15e3737b98f262afe884705a29c08c541242234
施振勛 <chris@ccf.org.tw>
2017-06-25 16:32:37 +0800

    commit Fody 的資料綁定用法

--- PrismUnityApp1/PrismUnityApp1/ViewModels/MainPageViewModel.cs
+++ PrismUnityApp1/PrismUnityApp1/ViewModels/MainPageViewModel.cs
@@ -1,43 +1,18 @@
 ﻿using Prism.Commands;
-using Prism.Mvvm;
 using Prism.Navigation;
 using System.ComponentModel;
 
 namespace PrismUnityApp1.ViewModels
 {
-    public class MainPageViewModel : BindableBase, INavigationAware
+    public class MainPageViewModel : INotifyPropertyChanged, INavigationAware
     {
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        public string MyEntry { get; set; }
+
+        public string MyLabel { get; set; }
+
         public DelegateCommand HelloCommand { get; set; }
-
-        #region MyEntry
-
-        private string _MyEntry;
-
-        /// <summary>
-        /// PropertyDescription
-        /// </summary>
-        public string MyEntry
-        {
-            get { return this._MyEntry; }
-            set { this.SetProperty(ref this._MyEntry, value); }
-        }
-
-        #endregion MyEntry
-
-        #region MyLabel
-
-        private string _MyLabel;
-
-        /// <summary>
-        /// PropertyDescription
-        /// </summary>
-        public string MyLabel
-        {
-            get { return this._MyLabel; }
-            set { this.SetProperty(ref this._MyLabel, value); }
-        }
-
-        #endregion MyLabel
 
         public MainPageViewModel()
         {
