2f4e273c592f3c97af24574d33c78f0e74f8f85e
施振勛 <chris@ccf.org.tw>
2017-06-25 17:31:01 +0800

    commit 程式碼片段範例

--- PrismUnityApp1/PrismUnityApp1/ViewModels/NewPageViewModel.cs
+++ PrismUnityApp1/PrismUnityApp1/ViewModels/NewPageViewModel.cs
@@ -1,16 +1,108 @@
-﻿using Prism.Commands;
-using Prism.Mvvm;
-using System;
-using System.Collections.Generic;
-using System.Linq;
+﻿using Prism.Events;
+using Prism.Navigation;
+using Prism.Services;
+using System.ComponentModel;
+using System.Threading.Tasks;
 
 namespace PrismUnityApp1.ViewModels
 {
-    public class NewPageViewModel : BindableBase
+
+    public class ViewModelClassName : INotifyPropertyChanged, INavigationAware
     {
-        public NewPageViewModel()
+        public event PropertyChangedEventHandler PropertyChanged;
+
+        #region Repositories (遠端或本地資料存取)
+
+        #endregion
+
+        #region ViewModel Property (用於在 View 中作為綁定之用)
+
+        #region 基本型別與類別的 Property
+
+        #endregion
+
+        #region 集合類別的 Property
+
+        #endregion
+
+        #endregion
+
+        #region Field 欄位
+
+        #region ViewModel 內使用到的欄位
+        #endregion
+
+        #region 命令物件欄位
+        #endregion
+
+        #region 注入物件欄位
+        public readonly IPageDialogService _dialogService;
+        private readonly INavigationService _navigationService;
+        private readonly IEventAggregator _eventAggregator;
+        #endregion
+
+        #endregion
+
+        #region Constructor 建構式
+        public ViewModelClassName(INavigationService navigationService, IEventAggregator eventAggregator,
+            IPageDialogService dialogService)
+        {
+
+            #region 相依性服務注入的物件
+
+            _dialogService = dialogService;
+            _eventAggregator = eventAggregator;
+            _navigationService = navigationService;
+            #endregion
+
+            #region 頁面中綁定的命令
+
+            #endregion
+
+            #region 事件聚合器訂閱
+
+            #endregion
+        }
+
+        #endregion
+
+        #region Navigation Events (頁面導航事件)
+        public void OnNavigatedFrom(NavigationParameters parameters)
         {
 
         }
+
+        public void OnNavigatingTo(NavigationParameters parameters)
+        {
+
+        }
+
+        public async void OnNavigatedTo(NavigationParameters parameters)
+        {
+            await ViewModelInit();
+        }
+        #endregion
+
+        #region 設計時期或者執行時期的ViewModel初始化
+        #endregion
+
+        #region 相關事件
+        #endregion
+
+        #region 相關的Command定義
+        #endregion
+
+        #region 其他方法
+
+        /// <summary>
+        /// ViewModel 資料初始化
+        /// </summary>
+        /// <returns></returns>
+        private async Task ViewModelInit()
+        {
+            await Task.Delay(100);
+        }
+        #endregion
     }
+
 }
